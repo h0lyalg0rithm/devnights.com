67b93891bd671c71f527ddb78b690201f9b43f25
Suraj Shirvankar <surajshirvankar@gmail.com>
2014-04-23 13:19:22 +0400

    Add created_at and updated_at to event model

--- app/models/event.js
+++ app/models/event.js
@@ -1,4 +1,3 @@
-// Example model
 
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
@@ -6,12 +5,34 @@ var mongoose = require('mongoose'),
 var EventSchema = new Schema({
   title: String,
   url: String,
-  text: String
+  text: String,
+  fbid: String,
+  meetupid: String,
+  eventbriteid: String,
+  created_at: {
+    type: Date,
+    default: Date.now()
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 EventSchema.virtual('date')
   .get(function(){
     return this._id.getTimestamp();
   });
+EventSchema.post('save', function(next){
+  this.updated_at = new Date();
+  if(!this.created_at){
+    this.created_at = new Date();
+  }
+  next();
+}).post('update' ,function(next){
+  this.updated_at = new Date();
+  next();
+});
+
 
 mongoose.model('Event', EventSchema);
